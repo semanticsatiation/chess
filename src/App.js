a4697d888b7d3ba5e9e3a3779238403ade7a5310
Benjamin <ben4853@gmail.com>
2021-12-15 17:34:03 -0500

    implement castling rough draft

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import Piece from './components/piece';
 import './stylesheets/chess.css';
 import { faChessBishop, faChessKing, faChessPawn, faChessKnight, faChessRook, faChessQueen} from '@fortawesome/free-solid-svg-icons';
 
+
 const defaultBoard = [
-  [ [3, 3], "r", "b" ],   [ [0, 1], "kn", "b" ],
-  [ [0, 2], "b", "b" ],   [ [2, 4], "q", "b" ],
-  [ [0, 4], "kg", "b" ],  [ [0, 5], "b", "b" ],
-  [ [0, 6], "kn", "b" ],  [ [0, 7], "r", "b" ],
-  [ [2, 1], "p", "b" ],   [ [1, 1], "p", "b" ],
+  [ [0, 0], "r", "b" ],  [ [0, 1], "kn", "b" ],
+  [ [0, 6], "kn", "b" ], [ [0, 7], "r", "b" ],   
+  [ [0, 2], "b", "b" ],  [ [0, 3], "q", "b" ],
+  [ [0, 4], "kg", "b" ], [ [0, 5], "b", "b" ],
+  [ [1, 0], "p", "b" ],  [ [1, 1], "p", "b" ],
   [ [1, 2], "p", "b" ],  [ [1, 3], "p", "b" ],
   [ [1, 4], "p", "b" ],  [ [1, 5], "p", "b" ],
   [ [1, 6], "p", "b" ],  [ [1, 7], "p", "b" ],
-  [ [1, 0], "p", "w" ],  [ [6, 1], "p", "w" ],
+  [ [6, 0], "p", "w" ],  [ [6, 1], "p", "w" ],
   [ [6, 2], "p", "w" ],  [ [6, 3], "p", "w" ],
   [ [6, 4], "p", "w" ],  [ [6, 5], "p", "w" ],
   [ [6, 6], "p", "w" ],  [ [6, 7], "p", "w" ],
   [ [7, 0], "r", "w" ],  [ [7, 1], "kn", "w" ],
   [ [7, 2], "b", "w" ],  [ [7, 3], "q", "w" ],
-  [ [7, 4], "kg", "w" ], [ [3, 5], "b", "w" ],
-  [ [4, 2], "kn", "w" ], [ [7, 7], "r", "w" ]
+  [ [7, 4], "kg", "w" ], [ [5, 3], "b", "w" ],
+  [ [5, 2], "kn", "w" ], [ [7, 7], "r", "w" ]
 ];
 
 const pieceCharacteristics = {
@@ -72,27 +73,98 @@ function App() {
   });
 
   const [gameState, setGameState] = useState({
-    currentTurn: "b",
+    currentTurn: "w",
     currentPiece: undefined,
 
     // currentlyEnPassantable should be set to undefined when the turn comes back to 
     // the player that moved their pawn and caused the currentlyEnPassantable to be not undefined
-    currentlyEnPassantable: undefined,
+    // currentlyEnPassantable[0] is black and obviously white is [1]
+    currentlyEnPassantable: [[], []],
     validPositions: [],
-    deadPieces: [[], []]
+    dangerZones: new Set([]),
+    deadPieces: [[], []],
+    castlelable: {
+      "b": [0, 4, 7],
+      "w": [56, 60, 63]
+    }
   });
 
   useEffect(() => {
     if (gameState.currentPiece !== undefined && gameState.currentPiece[2] === gameState.currentTurn) {
       markValidPositions();
     } else {
-      setGameState({
-        ...gameState,
+      setGameState((state) => ({
+        ...state,
         currentPiece: undefined,
-        validPositions: []
-      });
+        validPositions: [],
+      }));
     }
   }, [gameState.currentPiece]);
+
+  useEffect(() => {
+    setGameState((state) => ({
+      ...state,
+      currentPiece: undefined,
+      validPositions: [],
+      dangerZones: markDangerZones()
+    }));
+  }, [gameState.currentTurn]);
+
+
+  const markDangerZones = () => {
+    const dangerZones = new Set([]);
+
+    boardState.forEach((piece) => {
+      const pieceTraits = pieceCharacteristics[piece[1]];
+
+      if (gameState.currentTurn !== piece[2]) {
+        if (pieceTraits.moveMoreThanOneBlock) {
+          addBadPositions(dangerZones, pieceTraits.moveSet, piece[0], true);
+        } else {
+          piece[1] === "p" ? (
+            piece[2] === "b" ? (
+              addBadPositions(dangerZones, [pieceTraits.moveSet[0][2], pieceTraits.moveSet[0][3]], piece[0])
+            ) : (
+              addBadPositions(dangerZones, [pieceTraits.moveSet[1][2], pieceTraits.moveSet[1][3]], piece[0])
+            )
+          ) : (
+            addBadPositions(dangerZones, pieceTraits.moveSet, piece[0])
+          )
+        }
+      }
+    });
+
+    return new Set(dangerZones);
+  }
+
+  const addBadPositions = (endArr, moves, pos, isRecursion = false) => {
+    if (isRecursion) {
+      moves.forEach((move) => {
+        const newPos = [move[0] + pos[0], move[1] + pos[1]];
+
+        if (isWithinBoardGrid(newPos)) {
+          endArr.add(indexPiece(newPos));
+        }
+
+        
+        if (!isWithinBoardGrid(newPos) || findPiece(newPos) !== undefined) {
+          return;
+        }
+
+        addBadPositions(endArr, [move], newPos, isRecursion);
+      });
+    } else {
+      moves.forEach((move) => {
+        const newPos = [move[0] + pos[0], move[1] + pos[1]];
+
+        if ((!isWithinBoardGrid(newPos))) {
+          return;
+        }
+
+        endArr.add(indexPiece(newPos));
+      });
+    }
+  };
 
   const markValidPositions = () => {
     // pawns should have the choice of moving one or two spaces ON THE FIRST MOVE,
@@ -104,18 +176,30 @@ function App() {
 
     const validMoves = [];
 
+    const castleArr = gameState.castlelable[gameState.currentTurn];
+
     if (pieceTraits.moveMoreThanOneBlock) {
       addPositions(validMoves, pieceTraits.moveSet, piece[0], true);
     } else {
-      piece[1] === "p" ? (
+      if (piece[1] === "p") {
         piece[2] === "b" ? (
           addPositions(validMoves, [...piece[0][0] === 1 ? (pieceTraits.moveSet[0]) : ([pieceTraits.moveSet[0][0]]), pieceTraits.moveSet[0][2], pieceTraits.moveSet[0][3]], piece[0])
         ) : (
           addPositions(validMoves, [...piece[0][0] === 6 ? (pieceTraits.moveSet[1]) : ([pieceTraits.moveSet[1][0]]), pieceTraits.moveSet[1][2], pieceTraits.moveSet[1][3]], piece[0])
         )
-      ) : (
-        addPositions(validMoves, pieceTraits.moveSet, piece[0])
-      )
+        // 7 and 119 are to say that the king is no longer in the castle array meaning it moved because 
+        // the sum of the two rook indices is equal to 7 (black) or 119 (white)
+      } else if (piece[1] === "kg" && ![7, 119].includes(castleArr[0] + castleArr[1])) {
+        if (castleArr.length === 3) {
+          addPositions(validMoves, [...pieceTraits.moveSet, [0, 2], [0, -2]], piece[0]);
+        } else if ([4, 116].includes(castleArr[0] + castleArr[1])) {
+          addPositions(validMoves, [...pieceTraits.moveSet, [0, 2]], piece[0]);
+        } else {
+          addPositions(validMoves, [...pieceTraits.moveSet, [0, -2]], piece[0]);
+        }
+      } else {
+        addPositions(validMoves, pieceTraits.moveSet, piece[0]);
+      }
     }
 
     setGameState({
@@ -145,13 +229,60 @@ function App() {
   
           newBoard.splice(targetTileInd, 1);
         }
+
+        const castleArr = gameState.castlelable[gameState.currentTurn];
+
+        // FIX THE REPETITIVE CODE HERE AND ALSO FIGURE OUT WHY WE STILL CAN JUMP TWO SPACES WITH 
+        // THE KING IF THE LEFT OR RIGHT ROOK MOVES
+        // ALSO WE CANT CASTLE IF THERE ARE DANGER SQUARES IN THE WAY ANDDDDD WE CANT BE IN CHECK
+        // JUST FIX CASLTING OVERALL, IT'S REALLY WEIRD NOW
+
+        if (["kg", "r"].includes(currentPiece[1]) && ![7, 119].includes(castleArr[0] + castleArr[1])) {
+          setGameState({
+            ...gameState,
+            currentTurn: gameState.currentTurn === "b" ? ("w") : ("b"),
+            currentPiece: undefined,
+            deadPieces: newDeadPieces,
+            castlelable: {
+              ...gameState.castlelable,
+              [gameState.currentTurn]: castleArr.filter((index) => index !== indexPiece(currentPiece[0]))
+            }
+          });
+
+          if (currentPiece[1] === "kg") {
+            const isLeftRook = [2, 58].includes(indexPiece(pos));
   
-        setGameState({
-          ...gameState,
-          currentTurn: gameState.currentTurn === "b" ? ("w") : ("b"),
-          currentPiece: undefined,
-          deadPieces: newDeadPieces
-        });
+            const rookInd = newBoard.findIndex((arr) => indexPiece(arr[0]) === (isLeftRook ? (castleArr[0]) : (castleArr[castleArr.length - 1])));
+  
+            
+            const newRookArr = [...newBoard[rookInd]];
+    
+            newBoard.splice(rookInd, 1);
+  
+            setBoardState([
+              ...newBoard,
+              [(isLeftRook ? ([newRookArr[0][0], newRookArr[0][1] + 3]) : ([newRookArr[0][0], newRookArr[0][1] - 2])), ...newRookArr.slice(1)],
+              [pos, ...currentPiece.slice(1)]
+            ]);
+          } else {
+            setBoardState([
+              ...newBoard,
+              [pos, ...currentPiece.slice(1)]
+            ]);
+          }
+        } else {
+          setGameState({
+            ...gameState,
+            currentTurn: gameState.currentTurn === "b" ? ("w") : ("b"),
+            currentPiece: undefined,
+            deadPieces: newDeadPieces
+          });
+
+          setBoardState([
+            ...newBoard,
+            [pos, ...currentPiece.slice(1)]
+          ]);
+        }
   
         if (currentPiece[1] === "p") {
           // if a pawn reaches the opponents side, give them the option to convert
@@ -162,11 +293,6 @@ function App() {
            });
           }
         }
-  
-        setBoardState([
-          ...newBoard,
-          [pos, ...currentPiece.slice(1)]
-        ]);
       } else {
         setGameState({
           ...gameState,
@@ -201,7 +327,7 @@ function App() {
         if (isValidMove(newPos)) {
           endArr.push(indexPiece(newPos));
 
-          if (findPiece(newPos)!== undefined && findPiece(newPos)[2] !== gameState.currentTurn) {
+          if (findPiece(newPos) !== undefined && findPiece(newPos)[2] !== gameState.currentTurn) {
             return;
           }
           
@@ -283,6 +409,8 @@ function App() {
     boardState.find((piece) => indexPiece(piece[0]) === indexPiece(pos))
   );
 
+  console.log(gameState.castlelable);
+
   return (
     <div className="chess-container">
       <header>
@@ -306,9 +434,10 @@ function App() {
                 let piece = findPiece([ind, i]);
 
                 const isMarked = gameState.validPositions.includes(index);
+                const isDangerous = gameState.dangerZones.has(index);
 
                 return (
-                  <div className={`${isMarked ? ("mark") : ("")} block ${gameState.currentPiece && index === indexPiece(gameState.currentPiece[0]) ? ("current-piece") : ("")}`} key={i} onClick={(e) => setCurrentPiece([ind, i], isMarked)}>{
+                  <div className={`${isDangerous ? ("danger") : ("")} ${isMarked ? ("mark") : ("")} block ${gameState.currentPiece && index === indexPiece(gameState.currentPiece[0]) ? ("current-piece") : ("")}`} key={i} onClick={(e) => setCurrentPiece([ind, i], isMarked)}>{
                       piece !== undefined ? (
                         <button className="flex-center">
                           <Piece
